aad6552259b76a1f6420cb368630bb99531a6d28
dediindrawans <dediindrawan632@gmail.com>
2024-04-04 06:46:09 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='flex justify-center min-h-screen items-center bg-blue-600'>
+      <div>hello world!</div>
+    </div>
+  )
+}
+
+export default App
